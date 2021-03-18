403dbdd29b83e8efefe7703f7b182f6dc5f6f0bb
ubuntu <arpit170101012@iitg.ac.in>
2021-03-18 16:45:27 +0530

    --LINK argument reading done

--- Iscan/main.cpp
+++ Iscan/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char* argv[])
             iscan *IS = new iscan(stof(argv[3]), stoi(argv[4]), G);
             IS->executeSCAN();
             G->printClusters();
-            cout<<"Verteices information"<<endl;
+            cout<<"Vertices information"<<endl;
             G->printVertices();
 
             cout<<"Initial clustering done."<<endl;
@@ -117,6 +117,91 @@ int main(int argc, char* argv[])
             iscan *IS = new iscan(stof(argv[3]), stoi(argv[4]), G);
             IS->executeSCAN();
             G->printClusters();
+
+
+            cout<<"Vertices information"<<endl;
+            G->printVertices();
+
+            cout<<"Initial clustering done."<<endl;
+            cout<<"Number of edges to add/delete:"<<endl;
+            
+            int numupdates, update, src, dest;
+            cin>>numupdates;
+            for(int i=0;i<numupdates;i++)
+            {
+                // Check if vertex exists or not and same for edge
+                cin>>update>>src>>dest;
+                IS->updateEdge(src, dest, update);   
+                // G->printVertices();
+            }
+
+            G->printClusters();
+        }
+    }
+
+    if(strcmp(argv[1], "--LINK") == 0)
+    {
+        ifstream F(argv[2]);
+        if (!F) perror ("Error opening file");
+        else
+        {
+            graph* G = new graph();
+            string line;
+            int nedges = 0;
+            while(getline(F, line)){nedges++;}
+            F.clear();
+            F.seekg(0);
+            int id1, id2;
+            for(int i=0;i<nedges;i++)
+            {
+                F>>id1>>id2;
+                if(G->vertexMap.find(id1)==G->vertexMap.end())
+                {
+                    G->addVertex(id1, "");
+                }
+                if(G->vertexMap.find(id2)==G->vertexMap.end())
+                {
+                    G->addVertex(id2, "");
+                }
+                G->addEdge(id1, id2);
+            }
+
+            G->printGraph();
+
+            
+            if(stof(argv[3])>1 || stof(argv[3])<=0){cout<<"Epsilon value should be between 0 and 1"<<endl;exit(0);}
+            if(stoi(argv[4])<=0){cout<<"Mu value should be greater than 0"<<endl;exit(0);}
+
+            // create clusters and generates hubs and outliers
+            iscan *IS = new iscan(stof(argv[3]), stoi(argv[4]), G);
+            IS->executeSCAN();
+            G->printClusters();
+
+
+            cout<<"Vertices information"<<endl;
+            G->printVertices();
+
+            cout<<"Initial clustering done."<<endl;
+            cout<<"Number of edges to add/delete:"<<endl;
+            
+            int numupdates, update, src, dest;
+            cin>>numupdates;
+            for(int i=0;i<numupdates;i++)
+            {
+                // Check if vertex exists or not and same for edge
+                cin>>update>>src>>dest;
+                IS->updateEdge(src, dest, update);   
+                // G->printVertices();
+            }
+
+            G->printClusters();
         }
     }
 }
+
+/*
+    Dataset links:
+        http://snap.stanford.edu/data/index.html
+        http://law.di.unimi.it/datasets.php
+
+*/
